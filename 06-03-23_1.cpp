06c4a0d0a32786de76ddca9b4c4f37ae789c05c3
[Ranjit534] <ranjit121220@gmail.com>
2023-03-12 16:08:40 +0530

    1st Question

--- 06-03-23_1.cpp
+++ 06-03-23_1.cpp
@@ -5,9 +5,9 @@ public:
     {
         for (int i = 31; i >= 2; i--)
         {
-            int one = (1 << (i));                                     // largest bit
-            int two = (1 << (i - 1));                                 // 2nd largest bit
-            int three = (1 << (i - 2));                               // 3rd largest bit
+            int one = (1 << (i));                                     // largest value bit
+            int two = (1 << (i - 1));                                 // 2nd largest value bit
+            int three = (1 << (i - 2));                               // 3rd largest value bit
             if ((n & one) != 0 && (n & two) != 0 && (n & three) != 0) // If all 3 consecutive bits are one then make the 3rd largest bit 0 to get maximum value of n
                 n = n ^ (three);
         }
